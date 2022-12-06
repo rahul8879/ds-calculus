867415f390482ec4e8d03df324118133439a7ac0
unknown <rtiwarirahul123@gmail.com>
2022-12-07 00:11:09 +0530

    limit function added

--- 1.function_basic.py
+++ 1.function_basic.py
@@ -8,10 +8,24 @@ Created on Tue Dec  6 21:57:15 2022
 
 #1/0 problem
 
+from sympy import limit, Symbol
 
+# function to calculate the limit of a function 
 def test_v(input_1):
-    result = 1/(input_1-2)
-    return result
+    #credit : https://bobbyhadz.com/blog/python-zerodivisionerror-float-division-by-zero
+    # Different types of indeterminant forms : https://byjus.com/maths/indeterminate-forms/
+    try:
+        # if 1/0 
+        result = 1/(input_1-2)
+        return result
+    except ZeroDivisionError:
+        x = Symbol('x')
+        y=1/(x-2)**2
 
-test_v(1)
+        result = limit(y, x, 0)
+        
+        return result
+     
+
+#test_v(1)
 print(test_v(2))
