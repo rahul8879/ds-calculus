f81a062e66c8541e231286aeeb4f6a927ec5b76d
unknown <rtiwarirahul123@gmail.com>
2022-12-06 22:19:11 +0530

    'first_funtion'

---
+++ 1.function_basic.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  6 21:57:15 2022
+
+@author: Rahul
+"""
+
+
+#1/0 problem
+
+
+def test_v(input_1):
+    result = 1/(input_1-2)
+    return result
+
+test_v(1)
+print(test_v(2))
